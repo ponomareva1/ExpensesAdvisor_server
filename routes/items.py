8f5bd973298b6d087a8dda5dd55a7dc6544e9a8c
Elena Ponomareva <Elena.Ponomareva@emc.com>
2018-12-01 00:05:25 +0300

    add updateCategory request

--- routes/items.py
+++ routes/items.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 from flask_restful import fields, marshal
 
 from main import auth, checks, invalid_input
@@ -33,3 +33,32 @@ def get_items_from_check(check_id):
         items_list.append(marshalled_item)
 
     return jsonify({'items': items_list}), 200
+
+
+@items_route.route('/items/updateCategory', methods=['PUT'])
+@auth.login_required
+def update_items_category():
+    content = request.get_json(force=True, silent=True)
+    if any(key not in content for key in ('ids', 'category')):
+        return invalid_input("Invalid json format (not all fields are present)")
+
+    ids = content['ids']
+    if not ids:
+        return invalid_input("No ids provided.")
+    category = content['category']
+    if not isinstance(category, str):
+        return invalid_input("Category must be a string.")
+
+    # request to DB
+
+    for id in ids:
+        updated = False
+        for check in checks:
+            for item in check.items:
+                if item.id == id:
+                    item.category = category
+                    updated = True
+        if not updated:
+            return jsonify({'error': "Item with the ID={} was not found.".format(id)}), 404
+
+    return jsonify({'message': "Items category updated."}), 200
