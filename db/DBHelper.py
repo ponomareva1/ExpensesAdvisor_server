87dd21513abbfd2c1daeba28ac505648891cea81
veronikaKochugova <veronika.kochugova@gmail.com>
2018-12-15 14:27:03 +0300

    refactoring

--- db/DBHelper.py
+++ db/DBHelper.py
@@ -25,13 +25,13 @@ class DBHelper:
     # User API
     #
     def users(self):
-        return self.__selectAllQuery(USERS_TABLE)
+        return self.__select_all_query(USERS_TABLE)
 
     def add_user(self, login, password):
         # TODO: login&pass checking
 
         values = "('" + login + "','" + password + "')"
-        self.__insertQuery(USERS_TABLE, "(login,password)", values)
+        self.__insert_query(USERS_TABLE, "(login,password)", values)
         return self.user_id(login)
 
     def user_exist(self, login):
@@ -45,29 +45,29 @@ class DBHelper:
         return self.db.query(query)[0][0]
 
     def user_id(self, login):
-        return self.__selectQuery("id", USERS_TABLE, "WHERE login = '%s'" % login)[0][0]
+        return self.__select_query("id", USERS_TABLE, "WHERE login = '%s'" % login)[0][0]
 
     #
     # Checks API
     #
     def checks(self):
-        return self.__selectAllQuery(CHECKS_TABLE)
+        return self.__select_all_query(CHECKS_TABLE)
 
     def add_check(self, specifier, shop, date, user_id):
-        self.__insertQuery(CHECKS_TABLE,
+        self.__insert_query(CHECKS_TABLE,
                            "(specifier,shop,date,id_user)",
                            "('%s','%s','%s',%d)" % (specifier, shop, date, user_id)
-                           )
+                            )
         return self.check_id(specifier)
 
     def get_last_checks(self, n, login):
         if (not self.user_exist(login)):
             raise Exception("User with login '%s' doesn't exist" % login)
         userId = self.user_id(login)
-        return self.__selectTopQuery(n, CHECKS_TABLE, constraint="WHERE id_user = %d" % userId)
+        return self.__select_top_query(n, CHECKS_TABLE, constraint="WHERE id_user = %d" % userId)
 
     def check_id(self, specifier):
-        return self.__selectQuery("id", CHECKS_TABLE, "WHERE specifier = '%s'" % specifier)[0][0]
+        return self.__select_query("id", CHECKS_TABLE, "WHERE specifier = '%s'" % specifier)[0][0]
 
     #
     # Items API
@@ -78,16 +78,16 @@ class DBHelper:
                         JOIN %s ch ON ch.id = i.id_check 
                         JOIN %s c ON c.id = i.id_category""" % (ITEMS_TABLE, CHECKS_TABLE, CATEGORIES_TABLE)
         constraint = "WHERE ch.id = %d" % check_id
-        return ItemInfo(self.__selectQuery(columns, tables, constraint)[0])
+        return ItemInfo(self.__select_query(columns, tables, constraint)[0])
 
     # TODO: test for method
     def add_item(self, name, price, quant, check_id, category_id):
-        self.__insertQuery(ITEMS_TABLE, "(name,price,quant,id_category,id_check)",
+        self.__insert_query(ITEMS_TABLE, "(name,price,quant,id_category,id_check)",
                            "('%s',%d,%d,%d,%d)" % (name, price, quant, check_id, category_id))
         return self.item_id(name, check_id)
 
     def item_id(self, name, check_id):
-        return self.__selectQuery("id", ITEMS_TABLE, "WHERE name = '%s' "
+        return self.__select_query("id", ITEMS_TABLE, "WHERE name = '%s' "
                                                      "AND id_check = %d" % (name, check_id))[0][0]
 
     #
@@ -100,13 +100,13 @@ class DBHelper:
         self.__query("UPDATE", ITEMS_TABLE, "SET id_category = " + id)
 
     def category_id(self, name):
-        return self.__selectQuery("id", CATEGORIES_TABLE)[0][0]
+        return self.__select_query("id", CATEGORIES_TABLE)[0][0]
 
     def categories(self):
-        return self.__selectAllQuery(CATEGORIES_TABLE)
+        return self.__select_all_query(CATEGORIES_TABLE)
 
     def add_category(self, name):
-        self.__insertQuery(CATEGORIES_TABLE, "(name)", "('%s')" % name)
+        self.__insert_query(CATEGORIES_TABLE, "(name)", "('%s')" % name)
         return self.category_id(name)
 
     #
@@ -123,17 +123,17 @@ class DBHelper:
         except pg.exceptions.UniqueError as e:
             logger.warn("UniqueError : %s" % query)
 
-    def __insertQuery(self, tableName, where, what):
+    def __insert_query(self, tableName, where, what):
         self.__query("INSERT INTO", tableName, where + " VALUES " + what)
 
-    def __selectQuery(self, columns, tableName, constraint=""):
+    def __select_query(self, columns, tableName, constraint=""):
         return self.__query("SELECT %s FROM " % columns, tableName, constraint)
 
-    def __selectTopQuery(self, n, tableName, columns="*", constraint=""):
-        return self.__selectQuery(columns, tableName, constraint + "LIMIT %d" % n)
+    def __select_top_query(self, n, tableName, columns="*", constraint=""):
+        return self.__select_query(columns, tableName, constraint + "LIMIT %d" % n)
 
-    def __selectAllQuery(self, tableName, constraint=""):
-        return self.__selectQuery("*", tableName, constraint)
+    def __select_all_query(self, tableName, constraint=""):
+        return self.__select_query("*", tableName, constraint)
 
     def __create_new_db(self):
         connection = pg.open('pq://' + USER + ':' + PASS + '@' + HOST + ':' + PORT)
